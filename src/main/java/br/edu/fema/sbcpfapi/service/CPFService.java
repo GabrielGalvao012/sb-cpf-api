0405dd8a75b0d07d48d2ec35ce3c999166db7c24
gGalvão <114706794+GabrielGalvao012@users.noreply.github.com>
2023-06-16 21:08:14 -0300

    Validação de CPF

--- src/main/java/br/edu/fema/sbcpfapi/service/CPFService.java
+++ src/main/java/br/edu/fema/sbcpfapi/service/CPFService.java
@@ -4,33 +4,37 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CPFService {
-    public String validar(String cpf){
+    public String validar(String cpf) {
+        if (cpf.length() == 11) {
+
+
         int soma = 0;
         int soma2 = 0;
         int primeiroDigito;
         int segundoDigito;
-        for ( int i = 0; i < 9; i++){
-          int number = Integer.parseInt(cpf.substring(i, i + 1));
-            number =  number * (10 - i);
-                soma = soma + number;
+        for (int i = 0; i < 9; i++) {
+            int number = Integer.parseInt(cpf.substring(i, i + 1));
+            number = number * (10 - i);
+            soma = soma + number;
         }
 
-        for ( int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             int number2 = Integer.parseInt(cpf.substring(i, i + 1));
-            number2 =  number2 * (11 - i);
-                soma2 = soma2 + number2;
+            number2 = number2 * (11 - i);
+            soma2 = soma2 + number2;
         }
 
 
-        if( soma%11 < 2 ){
+        if (soma % 11 < 2) {
             primeiroDigito = 0;
-        }  else primeiroDigito = 11 - (soma % 11);
+        } else primeiroDigito = 11 - (soma % 11);
 
-        if( soma2%11 < 2){
+        if (soma2 % 11 < 2) {
             segundoDigito = 0;
         } else segundoDigito = 11 - (soma2 % 11);
 
         return cpf + " validado";
+        } else return " Tamanho Invalido";
     }
 
     public static void main(String[] args) {
